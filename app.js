3f7b93af703fdd760dced1dde907181637889768
romain <niamor.lth@gmail.com>
2024-01-30 22:50:07 +0100

    update

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
 var app = express();
-const allowedOrigins = ["https://zoka-rouge.vercel.app/"];
+const allowedOrigins = ["https://zoka-rouge.vercel.app"];
 app.use(
   cors({
     origin: function (origin, callback) {
